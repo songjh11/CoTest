2a3e8224711d948249317b344f1f182bcaa6813b
songjh11 <jhmelody91@gmail.com>
2023-01-19 17:30:23 +0900

    0119

--- src/com/cotest/main/Main208.java
+++ src/com/cotest/main/Main208.java
@@ -3,24 +3,25 @@ package com.cotest.main;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main207 {	
+public class Main208 {	
 	
-	public int solution(int n, ArrayList<Integer> numArr){
+	public ArrayList<Integer> solution(int n, ArrayList<Integer> numArr){
 		int answer = 0;
-		int answer2 = 0;
+		int lank = 1;
+		ArrayList<Integer> ansArr = new ArrayList<>();
 		for(int i=0; i<numArr.size(); i++) {
-			if(numArr.get(i)==1) {
-				answer++;
-				answer2 = answer2+answer;
-				System.out.println(answer);
-			} else {
-				answer = 0;
+			for(int j=0; j<numArr.size(); j++) {
+				if(numArr.get(j)>numArr.get(i)) {
+					lank++;
+				} 				
 			}
+			ansArr.add(lank);
+			lank = 1;
 		}
-		return answer2;
+		return ansArr;
 	}		
 	public static void main(String[] args){
-		Main207 t = new Main207();
+		Main208 t = new Main208();
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
 		ArrayList<Integer> numArr = new ArrayList<>();
@@ -31,6 +32,9 @@ public class Main207 {
 				numArr.add(Integer.parseInt(str));			
 			}
 		}
-			System.out.print(t.solution(n, numArr));
+		
+		for(Integer x : t.solution(n, numArr)) {
+			System.out.print(x+" ");			
+		}
 	}
 }
---
+++ src/com/cotest/main/Main210.java
@@ -0,0 +1,41 @@
+package com.cotest.main;
+
+import java.util.Scanner;
+
+public class Main210 {	
+	
+	public int solution(int n, int[][] numArr){
+		int answer = Integer.MIN_VALUE;
+		int sum1, sum2;
+		for(int i=0; i<n; i++) {
+				sum1=sum2=0;
+				for(int j=0; j<n; j++) {
+					sum1+=numArr[i][j];
+					sum2+=numArr[j][i];
+				}
+				answer=Math.max(answer, sum1);
+				answer=Math.max(answer, sum2);
+			}
+			sum1=sum2=0;
+			for(int i=0; i<n; i++) {
+				sum1+=numArr[i][i];
+				sum2+=numArr[i][n-i-1];
+			}
+			answer=Math.max(answer, sum1);
+			answer=Math.max(answer, sum2);
+		return answer;
+	}		
+	public static void main(String[] args){
+		Main210 t = new Main210();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[][] numArr = new int[n][n];
+		String str = "";
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				numArr[i][j] = kb.nextInt();		
+			}
+		}
+			System.out.print(t.solution(n, numArr));			
+		}
+}
